a28bacc5efe0183b139e8ed1d7fcbb0af49859d3
koncsik.mark <koncsik.mark@ckik.hu>
2024-04-24 08:47:01 +0200

    asd

--- alapvizsga.py
+++ alapvizsga.py
@@ -13,9 +13,29 @@ def befajl(nevek, pontok ,eredmeny):
 
 
 
+
+
+
+
+
+
+def csere(x, i, j):
+    x[i], x[j] = x[j], x[i]
+
+def rendez(pontok,nevek,eredmeny):
+    n = len(pontok)
+    for i in range(n):
+        for j in range(n-i-1):
+            if pontok[j] > pontok[j+1]:
+                csere(pontok, j, j+1)
+                csere(nevek, j, j+1)
+                csere(eredmeny, j, j+1)
+
+
 def main():
     nevek, pontok , eredmeny= [], [] ,[]
     maxpont=befajl(nevek, pontok,eredmeny)
+    rendez(nevek, pontok,eredmeny)
     print(nevek)
     print(pontok)
     print(eredmeny)
