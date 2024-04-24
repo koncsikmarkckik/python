b9f52c17acd8609bdd2cf5cc9ab86aeb32c5bc7a
urban.daniel <urban.daniel@ckik.hu>
2024-04-24 08:44:15 +0200

    haladunk

--- alapvizsga.py
+++ alapvizsga.py
@@ -3,7 +3,7 @@ def befajl(nevek, pontok ,eredmeny):
     maxpont = int(fr.readline().strip())
     sor = fr.readline().strip()
     while sor != "":
-        adatok = sor.split("; ")
+        adatok = sor.split(" ; ")
         nevek.append(adatok[0])
         pontok.append(int(adatok[1]))
         eredmeny.append(adatok[2])
@@ -12,11 +12,23 @@ def befajl(nevek, pontok ,eredmeny):
     return maxpont
 
 
+def sikeres_vizsgak(eredmeny):
+    db = 0
+    n = len(eredmeny)
+    for i in range(n):
+        if eredmeny[i] == "igen":
+            db += 1
+
+    return db
 
 def main():
     nevek, pontok , eredmeny= [], [] ,[]
-    maxpont=befajl(nevek, pontok,eredmeny)
+    maxpont = befajl(nevek, pontok, eredmeny)
+    db = sikeres_vizsgak(eredmeny)
     print(nevek)
     print(pontok)
     print(eredmeny)
+    print(db)
+
+
 main()
