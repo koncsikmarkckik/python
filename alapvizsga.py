521c3efbd051d8e9405dbdccf258bec5824960b2
kerier <koncsikm07@gmail.com>
2024-04-26 19:06:38 +0200

    round

--- alapvizsga.py
+++ alapvizsga.py
@@ -145,7 +145,7 @@ def main():
     print("Az átjutottak nevei:", atjutottak)
     print("A legtöbb pontot elért tanulo:", maxi)
     print("A legkevesebbet pontot elért tanulo:", mini)
-    print("A teljesítmény átlaga:", atl)
+    print("A teljesítmény átlaga:", round(atl,2))
 
 
 main()
